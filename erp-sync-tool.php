d0c5e1e98b0624ec5cdb1e83b6af05abe8228dc8
Liam S <liam.s@pvtl.io>
2021-04-08 08:54:44 +1000

    Restrict customer email sending based on the email_on_create get param

--- erp-sync-tool.php
+++ erp-sync-tool.php
@@ -28,9 +28,31 @@ class ErpSyncTool {
 	/**
 	 * Constructor
 	 */
-	public function __construct() {
+	public function __construct() 
+    {
 		add_filter( 'woocommerce_rest_customer_query', array( $this, 'add_updated_since_filter_to_rest_api' ), 100, 2 );
-	}
+        add_action( 'woocommerce_created_customer', array ( $this, 'woocommerce_send_email_on_rest_create'), 10, 2 );
+    }
+
+    /**
+     * Restricts sending of emails for new customer accounts if the 
+     * 'email_on_create' param is present on the get params of the request. 
+     */
+    public function woocommerce_send_email_on_rest_create( $customerId )
+    {
+        if (!isset($_GET['email_on_create'])) {
+            return;
+        }
+
+        add_filter( 'woocommerce_email_enabled_customer_new_account', function() {
+            /**
+             * @var bool $enabled
+             * @var \WP_User $user
+             * @var \WC_Email_Customer_Completed_Order $email
+             */
+             return $_GET['email_on_create'] === 'true' ? true : false;
+        }, 10, 3 );
+    }
 
     /**
      * Adds the 'updated since' get filter on rest api customer requests to woocommerce
