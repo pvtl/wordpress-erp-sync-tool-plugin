b36253f0d93e5e7b7d15b49c5b6ec4cfbef17da2
Liam Southwell <liam.a.southwell@gmail.com>
2021-06-07 15:30:56 +1000

    Support image file uploads with an image link and image_name property

--- classes/class-erp-sync-tool.php
+++ classes/class-erp-sync-tool.php
@@ -31,10 +31,10 @@ class Erp_Sync_Tool {
 	/**
 	 * Constructor
 	 */
-	public function __construct() 
+	public function __construct()
     {
         new Erp_Sync_Tool_Settings();
-        
+
         add_action('woocommerce_new_order', array( $this, 'dispatch_order_sync_on_order' ), 10, 1);
         add_action('woocommerce_update_order', array( $this, 'dispatch_order_sync_on_order' ), 10, 1);
 
@@ -42,6 +42,59 @@ class Erp_Sync_Tool {
         add_action( 'woocommerce_created_customer', array ( $this, 'woocommerce_customer_creation'), 10, 2 );
 
         add_filter( "woocommerce_rest_shop_order_object_query", array ( $this, 'add_orders_updated_since_filter' ) , 10, 2 );
+
+        // Add image if exists
+        add_action( 'woocommerce_rest_insert_product_object', array( $this, 'upload_image_where_exists' ), 10, 2 );
+    }
+
+
+    public function upload_image_where_exists( $product, $request )
+    {
+        $json = $request->get_json_params();
+        if (isset($json['image'])) {
+            $post_id = $product->get_id();
+            // Include files so we can sideload
+            require_once( ABSPATH . 'wp-admin/includes/media.php' );
+            require_once( ABSPATH . 'wp-admin/includes/file.php' );
+            require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+            // Get an image name so we can avoid adding duplicates
+            $image_name = isset($json['image_name']) ? $json['image_name'] : '';
+
+            // Sideload image & add to media library
+            $media = media_sideload_image( 
+                $json['image'], 
+                $post_id,
+                $image_name
+            );
+            
+            // If we uploaded an image
+            if(!empty($media) && !is_wp_error( $media )){
+                $args = array(
+                    'post_type' => 'attachment',
+                    'posts_per_page' => -1,
+                    'post_status' => 'any',
+                    'post_parent' => $post_id
+                );
+
+                // Grab the new image
+                $attachments = get_posts( $args );
+
+                if( isset( $attachments ) && is_array( $attachments ) ){
+                    foreach($attachments as $attachment){
+                        // Grab source of full size images (so no 300x150 nonsense in path)
+                        $image = wp_get_attachment_image_src( $attachment->ID, 'full' );
+                        // Determine if in the $media image we created, the string of the URL exists
+                        if( strpos( $media, $image[0]) !== false ){
+                            // If so, we found our image. set it as thumbnail
+                            set_post_thumbnail( $post_id, $attachment->ID );
+                            // Break out because we have our image
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public function dispatch_order_sync_on_order( $order_id )
@@ -50,14 +103,14 @@ class Erp_Sync_Tool {
 
         foreach ($options['services'] as $service) {
             if (strpos($service['service_type'], 'OrderSync') !== false) {
-                wp_remote_get( 
-                    Erp_Sync_Tool::$api_base .'sync/' . $service['webhook'], 
+                wp_remote_get(
+                    Erp_Sync_Tool::$api_base .'sync/' . $service['webhook'],
                     array(
-                        'headers' => array ( 
+                        'headers' => array (
                             'Accept' => 'Application/json',
                             'Authorization' => 'Bearer ' . $options['api_key'],
                         )
-                    ) 
+                    )
                 );
             }
         }
@@ -70,7 +123,7 @@ class Erp_Sync_Tool {
         if (isset($_GET['updated_since'])) {
             $prepared_args['date_query'][] = array(
                 'after' => $_GET['updated_since'],
-                'column' => 'post_modified' 
+                'column' => 'post_modified'
             );
         }
         return $prepared_args;
@@ -83,7 +136,7 @@ class Erp_Sync_Tool {
     }
 
     /**
-     * Sets the customer role on craete 
+     * Sets the customer role on craete
      */
     private function set_customer_user_role_on_create( $customer_id )
     {
@@ -96,8 +149,8 @@ class Erp_Sync_Tool {
     }
 
     /**
-     * Restricts sending of emails for new customer accounts if the 
-     * 'email_on_create' param is present on the get params of the request. 
+     * Restricts sending of emails for new customer accounts if the
+     * 'email_on_create' param is present on the get params of the request.
      */
     public function send_email_on_customer_create()
     {
@@ -129,7 +182,7 @@ class Erp_Sync_Tool {
                 ),
             );
         }
-        
+
         return $prepared_args;
     }
 
