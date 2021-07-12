2d79a98e7a6790cc819e949cd42423d2d6b6bf1c
Liam Southwell <liam.a.southwell@gmail.com>
2021-07-12 14:12:58 +1000

    Add root url option

--- classes/class-erp-sync-tool-settings.php
+++ classes/class-erp-sync-tool-settings.php
@@ -103,6 +103,7 @@ class Erp_Sync_Tool_Settings {
         }
         $newinput['api_key'] = trim( $input['api_key'] );
         $newinput['client_name'] = trim( $input['client_name'] );
+        $newinput['root_url'] = trim( $input['root_url'] );
 
         return $newinput;
     }
@@ -124,7 +125,7 @@ class Erp_Sync_Tool_Settings {
             && $_GET['page'] === 'erp-sync-tool' 
             && $options 
             && !empty( $options['api_key'] ) 
-            && !empty( $options['client_name'] ) 
+            && !empty( $options['client_name'] )
         ) {
             $this->refresh_service_settings( $options );
             $this->add_service_settings( $options );
@@ -225,13 +226,15 @@ class Erp_Sync_Tool_Settings {
     {
         add_settings_section( 'api_settings', 'API Settings', array( $this, 'api_section_text' ), 'erp_sync_tool' );
         add_settings_field( 'erp_sync_tool_setting_api_key', 'API Key', array( $this, 'api_key_setting_field'), 'erp_sync_tool', 'api_settings', $options );
+        add_settings_field( 'erp_sync_tool_setting_root_url', 'Root URL', array( $this, 'api_root_url_field'), 'erp_sync_tool', 'api_settings', $options );
         add_settings_field( 'erp_sync_tool_setting_client_name', 'Client Name', array( $this, 'client_name_setting_field'), 'erp_sync_tool', 'api_settings', $options );
     }
 
     public function refresh_service_settings( $options )
     {
+        $root = isset($options['root_url']) ? $options['root_url'] : Erp_Sync_Tool::$api_base;
         $result = wp_remote_get( 
-            Erp_Sync_Tool::$api_base .'services?client=' . $options['client_name'], 
+            $root .'services?client=' . $options['client_name'], 
             array(
                 'headers' => array ( 
                     'Accept' => 'Application/json',
@@ -274,6 +277,11 @@ class Erp_Sync_Tool_Settings {
     public function api_key_setting_field( $options ) 
     {
         echo "<input id='erp_sync_tool_options_api_key' name='erp_sync_tool_options[api_key]' type='text' value='" . esc_attr( $options['api_key'] ) . "' />";
+    }
+
+    public function api_root_url_field( $options ) 
+    {
+        echo "<input id='erp_sync_tool_options_root_url' name='erp_sync_tool_options[root_url]' type='text' value='" . esc_attr( $options['root_url'] ) . "' />";
     }
 
     public function client_name_setting_field( $options ) 
