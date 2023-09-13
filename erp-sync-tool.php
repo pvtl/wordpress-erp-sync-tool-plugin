05412e0dccf663ce9d9ec578e59ac022f70e0315
Kyle Stevenson <kyle.stevenson@gmail.com>
2023-09-13 13:54:05 +1000

    Remove duplicate comment block, bump version

--- erp-sync-tool.php
+++ erp-sync-tool.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Plugin Name:     ERP Sync Tool
- * Plugin URI:      https://github.com/pvtl
- * Description:     Links Pivotal's ERP Sync Tool to WooCommerce.
+ * Plugin URI:      https://github.com/pvtl/wordpress-erp-sync-tool-plugin
+ * Description:     A link between Pivotal's ERP Sync Tool & Woocommerce
  * Author:          Pivotal Agency
  * Author URI:      http://pivotal.agency
  * Text Domain:     erp-sync-tool
  * Domain Path:     /languages
- * Version:         0.0.7
+ * Version:         0.1.0
  *
  * @package         ERP Sync Tool
  */
@@ -26,17 +26,5 @@ require_once ERP_PATH . '/autoload.php';
 function ERP() { // phpcs:ignore WordPress.NamingConventions
 	return Classes\Erp_Sync_Tool::instance();
 }
-/**
- * Plugin Name:     ERP Sync Tool
- * Plugin URI:      https://github.com/pvtl/wordpress-erp-sync-tool-plugin
- * Description:     A link between Pivotal's ERP Sync Tool & Woocommerce
- * Author:          Pivotal Agency
- * Author URI:      http://pivotal.agency
- * Text Domain:     erp-sync-tool
- * Domain Path:     /languages
- * Version:         0.0.6
- *
- * @package         ERP_SYNC_TOOL
- */
 
 ERP();
